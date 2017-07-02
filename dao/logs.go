2d16001fc728251b1dccb9aab8f5acacb84470ba
Daniil Matkov <daniil.matkov@gmail.com>
2017-07-02 15:43:57 +0300

    GetByID method added to DAO

--- dao/logs.go
+++ dao/logs.go
@@ -20,3 +20,22 @@ func Insert(log models.Log) {
 	}
 	db.Close();
 }
+
+func GetByID(id int64) models.Log {
+	db, err := sql.Open("mysql", SQL_CONNECT_STRING)
+	if err != nil {
+		panic(err)
+	}
+	query := fmt.Sprintf("Select From,Status,Message from logs where ID=%d);", id)
+	rows, err := db.Query(query)
+	var from string
+	var status string
+	var message string
+	for rows.Next() {
+		if err := rows.Scan(&from, &status, &message); err != nil {
+			panic(err)
+		}
+	}
+	res := models.Log{id, from, status, message}
+	return res
+}
