66b738f832a8af84994eece26e9824637d697786
Daniil Matkov <daniil.matkov@gmail.com>
2017-07-02 16:23:59 +0300

    FindByID view fixed

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"./dao"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
-
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
@@ -14,8 +16,20 @@ func data(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "{'data':'working'}")
 }
 
+func findByID(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Path[1:])
+	if err != nil {
+		panic(err)
+	}
+	logs := dao.GetByID(int64(id))
+	logsMarshalled, err := json.Marshal(logs)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(w, string(logsMarshalled))
+}
 func main() {
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/data", data)
+	http.HandleFunc("/id", findByID)
 	http.ListenAndServe(":8080", nil)
 }
--- models/log.go
+++ models/log.go
@@ -1,8 +1,8 @@
 package models
 
 type Log struct {
-	ID      int64
-	From    string
-	Status  string
-	Message string
+	ID      int64  `json: "id"`
+	From    string `json: "from"`
+	Status  string `json: "status"`
+	Message string ` json: "message"`
 }
